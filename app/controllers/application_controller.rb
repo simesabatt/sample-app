a2c948bf88e40460d4f74a08a88c520e5c324f22
simesabatt <t95203631@gmail.com>
2020-11-10 14:30:10 +0000

    2. トップページタスク完了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
+  
+  def set_current_user
+    @current_user = User.find(session[:user_id])
+  end
 end
