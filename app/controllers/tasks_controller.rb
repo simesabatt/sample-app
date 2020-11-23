cfbc77b271adbe1085693bd009ccc16fa18768f7
simesabatt <t95203631@gmail.com>
2020-11-23 11:24:49 +0000

    削除機能＋バリデート途中

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,7 +7,7 @@ class TasksController < ApplicationController
   
   def create
     @user = current_user.id
-    # @task = @user.tasks.build(task_params) ←これが良いみたいだけど・・・
+    # @task = @user.tasks.build(task_params) ←これが良いみたいだけどうまく行かず・・・
     @task = Task.new(task_params)
     if @task.save
       flash[:success] = 'タスクを新規作成しました。'
@@ -19,20 +19,37 @@ class TasksController < ApplicationController
   end
   
   def index
-    @tasks = Task.all
+    @tasks = Task.all.order(created_at: :desc)
     @user = current_user.id
   end
   
   def show
     @task = Task.find(params[:id])
+    @user = current_user.id
   end
   
   def edit
     @task = Task.find(params[:id])
+    @user = current_user.id
   end
 
   def update
-    redirect_to user_tasks_url
+    @task = Task.find(params[:id])
+    @user = current_user.id
+    if @task.update_attributes(task_params)
+      flash[:success] = "タスクを更新しました。"
+      redirect_to user_task_url(@user, @task)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @user = current_user.id
+    @task.destroy
+    flash[:success] = "タスクを削除しました"
+    redirect_to user_tasks_url(@user, @task)
   end
 
   private
@@ -40,4 +57,5 @@ class TasksController < ApplicationController
     def task_params
       params.require(:task).permit(:task_title, :task_detail, :user_id)
     end
+
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,6 @@
 class Task < ApplicationRecord
   belongs_to :user
+  
+  validates :task_title, presence: true
+  validates :task_detail, presence: true
 end
